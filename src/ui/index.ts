a35d87942dfea6955b9442b8c30034edd95e7cd1
buropotok <buropotok@gmail.com>
2026-09-11 19:09:21 +0300

    Export UI Kit calendar

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,2 +1,3 @@
 export { Avatar, Button, Divider, IconButton, Surface, Text } from './primitives';
+export { Calendar, type CalendarProps } from './Calendar';
 export { FloatingActionButton, List, ListItem, Modal, Tabs, TabsContent, TabsList, TabsTrigger } from './components';
