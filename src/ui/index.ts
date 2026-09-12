f6d5949ab344715753018fdc7d072bc64fc075e0
buropotok <buropotok@gmail.com>
2026-09-12 17:11:29 +0300

    Export UI Kit SearchInput

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,4 +2,5 @@ export { Avatar, Button, Divider, IconButton, Surface, Text } from './primitives
 export { Calendar, type CalendarProps } from './Calendar';
 export { FloatingActionButton, List, ListItem, Modal, Tabs, TabsContent, TabsList, TabsTrigger } from './components';
 export { Menu, MenuDivider, MenuItem, type MenuProps } from './Menu';
+export { SearchInput, type SearchInputProps } from './SearchInput';
 export { SortableList, type SortableListItem, type SortableListProps } from './SortableList';
