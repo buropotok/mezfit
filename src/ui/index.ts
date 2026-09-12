aa9dc9b545b21cdc0d58fc085d2f225070a383d8
buropotok <buropotok@gmail.com>
2026-09-12 03:16:01 +0300

    Export UI kit menu

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,4 @@
 export { Avatar, Button, Divider, IconButton, Surface, Text } from './primitives';
 export { Calendar, type CalendarProps } from './Calendar';
 export { FloatingActionButton, List, ListItem, Modal, Tabs, TabsContent, TabsList, TabsTrigger } from './components';
+export { Menu, MenuDivider, MenuItem, type MenuProps } from './Menu';
