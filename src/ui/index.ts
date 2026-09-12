623f0a751bc505c9b91a9097907489768dd11625
buropotok <buropotok@gmail.com>
2026-09-13 00:26:28 +0300

    Export dropdown component

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,6 @@
 export { Avatar, Button, Divider, IconButton, Surface, Text } from './primitives';
 export { Calendar, type CalendarProps } from './Calendar';
+export { Dropdown, type DropdownOption, type DropdownProps, type MultiDropdownProps, type SingleDropdownProps } from './Dropdown';
 export { FloatingActionButton, List, ListItem, Modal, Tabs, TabsContent, TabsList, TabsTrigger } from './components';
 export { Menu, MenuDivider, MenuItem, type MenuProps } from './Menu';
 export { SearchInput, type SearchInputProps } from './SearchInput';
