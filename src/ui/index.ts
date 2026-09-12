9b4c796e0f6da99ac15c15d1053cd6c623e43345
buropotok <buropotok@gmail.com>
2026-09-12 07:20:00 +0300

    Export SortableList from UI Kit

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,3 +2,4 @@ export { Avatar, Button, Divider, IconButton, Surface, Text } from './primitives
 export { Calendar, type CalendarProps } from './Calendar';
 export { FloatingActionButton, List, ListItem, Modal, Tabs, TabsContent, TabsList, TabsTrigger } from './components';
 export { Menu, MenuDivider, MenuItem, type MenuProps } from './Menu';
+export { SortableList, type SortableListItem, type SortableListProps } from './SortableList';
