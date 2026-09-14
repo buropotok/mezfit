7dbd159194773a068f4ab074c87d1887ec9aa338
buropotok <buropotok@gmail.com>
2026-09-14 09:22:40 +0300

    Сделать миграцию программ безопасной для внешних ключей

--- migrations/0022_allow_multiple_training_plans.sql
+++ migrations/0022_allow_multiple_training_plans.sql
@@ -1,3 +1,7 @@
+-- D1 keeps foreign-key enforcement enabled during migrations. Defer checks until
+-- the replacement table has been renamed back to training_plan.
+PRAGMA defer_foreign_keys = ON;
+
 CREATE TABLE training_plan_next (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL,
@@ -17,3 +21,5 @@ ALTER TABLE training_plan_next RENAME TO training_plan;
 
 CREATE INDEX idx_training_plan_user_id ON training_plan(user_id);
 CREATE INDEX idx_training_plan_created_by_user_id ON training_plan(created_by_user_id);
+
+PRAGMA defer_foreign_keys = OFF;
