1d25ccc5666f2f1352c7df305f54ca984662410f
buropotok <buropotok@gmail.com>
2026-09-14 08:20:36 +0300

    Export TextInput contract

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,7 +2,7 @@ export { Avatar, Button, Divider, IconButton, Surface, Text } from './primitives
 export { Badge, type BadgeColor, type BadgeProps } from './Badge';
 export { Calendar, type CalendarProps } from './Calendar';
 export { Dropdown, type DropdownOption, type DropdownProps, type MultiDropdownProps, type SingleDropdownProps } from './Dropdown';
-export { Checkbox, Radio, TextInput, type CheckboxProps, type RadioProps } from './FormControls';
+export { Checkbox, Radio, TextInput, type CheckboxProps, type RadioProps, type TextInputProps } from './FormControls';
 export { FloatingActionButton, List, ListItem, Modal, Tabs, TabsContent, TabsList, TabsTrigger, type ModalAction } from './components';
 export { Menu, MenuDivider, MenuItem, type MenuProps } from './Menu';
 export { SearchInput, type SearchInputProps } from './SearchInput';
