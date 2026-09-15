20e795dff98c7e44e4197dbf7d6544ba6f76d981
buropotok <buropotok@gmail.com>
2026-09-15 21:40:10 +0300

    Export nested badges primitive

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -5,5 +5,6 @@ export { Dropdown, type DropdownOption, type DropdownProps, type MultiDropdownPr
 export { Checkbox, Radio, TextArea, TextInput, type CheckboxProps, type RadioProps, type TextAreaProps, type TextInputProps } from './FormControls';
 export { FloatingActionButton, List, ListItem, Modal, Tabs, TabsContent, TabsList, TabsTrigger, type ModalAction } from './components';
 export { Menu, MenuDivider, MenuItem, type MenuProps } from './Menu';
+export { NestedBadges, type NestedBadgeItem, type NestedBadgesProps } from './NestedBadges';
 export { SearchInput, type SearchInputProps } from './SearchInput';
 export { SortableList, type SortableListItem, type SortableListProps } from './SortableList';
