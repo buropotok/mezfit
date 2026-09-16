c5bc45ef6af5fff3483d3570ab511964be61344b
buropotok <buropotok@gmail.com>
2026-09-16 04:08:11 +0300

    Type persisted sets in coach program details

---
+++ src/coach/program-details-set-projection.d.ts
@@ -0,0 +1,14 @@
+import '../api';
+
+declare module '../api' {
+  interface ProgramPhaseExerciseDetails {
+    sets: Array<{
+      id: number;
+      setNumber: number;
+      reps: number | null;
+      weightKg: number | null;
+      durationSeconds: number | null;
+      distanceMeters: number | null;
+    }>;
+  }
+}
